a10c250fccb67f2a40e6fe17158883a97b1e5378
DiaboloKiat <DiaboloKiat@gmail.com>
2021-12-20 16:44:28 +0800

    update github on project_seadrone

--- github/git_branch.sh
+++ github/git_branch.sh
@@ -14,5 +14,8 @@ git checkout master
 
 ############################## submodules ####################################
 
+source ~/$REPO/catkin_ws/src/vrx/github/git_branch.sh master_thesis
+cd ~/$REPO
+
 source ~/$REPO/catkin_ws/src/pozyx_uwb/github/git_branch.sh master_thesis
 cd ~/$REPO
