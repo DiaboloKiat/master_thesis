bb23082e23087ece1140052141a4b67cefe4f9c0
DiaboloKiat <DiaboloKiat@gmail.com>
2022-04-25 22:56:41 +0800

    update on master_thesis

--- docker/laptop/ubuntu_18.04/docker_build.sh
+++ docker/laptop/ubuntu_18.04/docker_build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build --rm -t diabolokiat/kiat-thesis:laptop-ubuntu18.04 .
+docker build --rm --build-arg ssh_prv_key="$(cat ~/.ssh/id_ed25519)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_ed25519.pub)" -t diabolokiat/kiat-thesis:laptop-ubuntu18.04 .
